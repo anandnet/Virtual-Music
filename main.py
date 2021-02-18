f1606d15bf60bd47647342b2f247c67ec91b1ae0
anandnet <anandnet628@gmail.com>
2021-02-18 11:18:19 +0530

    #1

---
+++ main.py
@@ -0,0 +1,66 @@
+from utils import detector_utils as detector_utils
+import cv2
+import tensorflow as tf
+import datetime
+import argparse
+
+detection_graph, sess = detector_utils.load_inference_graph()
+
+if __name__ == '__main__':
+
+    fps=1 #for showing fps
+    width=320
+    height=2400
+    score_thresh=0.2 #Score threshold for displaying bounding boxes
+    display=1  #display image using opencv
+    cap = cv2.VideoCapture(0)
+    start_time = datetime.datetime.now()
+    num_frames = 0
+    im_width, im_height = (320,240)
+    # max number of hands we want to detect/track
+    num_hands_detect = 1
+
+    cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
+
+    while True:
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        ret, image_np = cap.read()
+        image_np=cv2.resize(image_np,(im_width,im_height))
+        # image_np = cv2.flip(image_np, 1)
+        try:
+            image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+        except:
+            print("Error converting to RGB")
+
+        # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
+        # while scores contains the confidence for each of these boxes.
+        # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
+
+        boxes, scores = detector_utils.detect_objects(image_np,
+                                                      detection_graph, sess)
+
+        # draw bounding boxes on frame
+        detector_utils.draw_box_on_image(num_hands_detect, score_thresh,
+                                         scores, boxes, im_width, im_height,
+                                         image_np)
+
+        # Calculate Frames per second (FPS)
+        num_frames += 1
+        elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
+        fps = num_frames / elapsed_time
+
+        if (display > 0):
+            # Display FPS on frame
+            if (fps > 0):
+                detector_utils.draw_fps_on_image("FPS : " + str(int(fps)),
+                                                 image_np)
+
+            cv2.imshow('Single-Threaded Detection',
+                       cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+        else:
+            print("frames processed: ", num_frames, "elapsed time: ",
+                  elapsed_time, "fps: ", str(int(fps)))
